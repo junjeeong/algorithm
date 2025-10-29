745b308190786152fbddf78e4316d9004e965841
junjeong <wn8624@naver.com>
2025-10-29 11:38:09 +0900

    edit boj2839

--- 1029/src/BOJ2839.java
+++ 1029/src/BOJ2839.java
@@ -6,21 +6,17 @@ public class BOJ2839 {
         int N = sc.nextInt();
         sc.close();
 
-        // 5kg 봉투를 최대 개수(N/5)부터 시작하여 0개까지 줄여나간다.
-        int maxFiveKg = N / 5; // N = 11 -> 2
-        int minBags = -1; // 최소 봉투 개수 (-1로 초기화)
+        int fiveBags = N/5; // N = 18 -> 3
 
-        for (int fiveKg = maxFiveKg; fiveKg >= 0; fiveKg--) {
-            int remaining = N - (fiveKg * 5); // 5kg 봉투를 사용하고 남은 무게 // 11 - 10 = 1
+        for(int minBags = fiveBags; minBags >=0; minBags--){
+            int remaining = N - (minBags * 5);
 
-            // 남은 무게가 3kg으로 나누어 떨어지는지 확인
-            if (remaining % 3 == 0) {
-                int threeKg = remaining / 3;
-                minBags = fiveKg + threeKg;
-                break; // 최소 개수를 찾았으므로 즉시 반복문 종료
+            if(remaining % 3 ==0) {
+                int threeBags = remaining / 3; // remaining = 3 -> 1
+                System.out.println(minBags + threeBags);
+                System.exit(0);
             }
         }
-
-        System.out.println(minBags);
+        System.out.println(-1);
     }
 }
