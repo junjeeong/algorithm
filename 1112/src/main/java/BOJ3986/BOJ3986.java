68c188e6e97381d4cb044b17f0de42afc2fbabfc
junjeong <wn8624@naver.com>
2025-11-12 18:48:25 +0900

    add 1112 boj3986 silver4

--- 1112/src/main/java/BOJ3986/BOJ3986.java
+++ 1112/src/main/java/BOJ3986/BOJ3986.java
@@ -1,9 +1,11 @@
+package BOJ3986;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class BOJ {
+public class BOJ3986 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
