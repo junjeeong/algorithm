9ec25395778c186f00fc7a8b72b8c8e43fce362f
junyeong <wn8624@naver.com>
2025-11-19 17:12:04 +0900

    chore: 디렉토리 정리

---
+++ java-algorithm-interview/1119/src/LEET125_v1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LEET125_v1 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String string = sc.nextLine().toUpperCase().replaceAll(" ", "");
+
+    for (int i = 0; i < string.length(); i++) {
+      if (i == string.length() - 1 - i) {
+        break;
+      }
+
+      if (string.charAt(i) == string.charAt(string.length() - 1 - i)) {
+        continue;
+      } else {
+        System.out.println(false);
+        System.exit(1);
+      }
+    }
+
+    System.out.println(true);
+  }
+}
---
+++ java-algorithm-interview/1119/src/LEET125_v2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LEET125_v2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.nextLine();
+
+    String filteredS = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+    String reverseS = new StringBuilder(filteredS).reverse().toString();
+    System.out.println(filteredS.equals(reverseS));
+  }
+}
