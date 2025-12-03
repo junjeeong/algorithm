b1415b2b428032cda06368b3e92372bd248529a6
junyeong <wn8624@naver.com>
2025-12-03 15:04:52 +0900

    add 1203

---
+++ java-algorithm-interview/Ch8_LinkedList/src/P19_역순연결리스트.java
@@ -0,0 +1,29 @@
+public class P19_역순연결리스트 {
+
+  public ListNode reverseBetween(ListNode head, int left, int right) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode root = new ListNode(0);
+
+    root.next = head;
+
+    ListNode start = root;
+
+    for (int i = 0; i < left - 1; i++) {
+      start = start.next;
+    }
+
+    ListNode end = start.next;
+
+    for (int i = 0; i < right - left; i++) {
+      ListNode tmp = start.next;
+      start.next = end.next;
+      end.next = end.next.next;
+      start.next.next = tmp;
+    }
+
+    return root.next;
+  }
+}
