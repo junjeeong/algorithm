560f2bbd572fbb5f9c38183717ada2152c9e3a06
junyeong <wn8624@naver.com>
2025-12-16 19:41:55 +0900

    add 1216

---
+++ java-algorithm-interview/CH11_HASH/P34_완주하지못한선수.java
@@ -0,0 +1,20 @@
+public String Solution(STring[] participant, String[] completion) {
+  Map<String, Integer> m = new HashMap<>();
+
+  for (String p : participant) {
+    m.put(p, m.getOrDefault(p, 0) + 1);
+  }
+
+  for (String c : completion) {
+    int left = m.get(c);
+
+    if (left == 1) {
+      m.remove(c);
+    } else {
+      m.put(c, left - 1);
+    }
+  }
+
+  return m.entrySet().iterator().next().getKey();
+
+}
