dadbb628131d58fbba3d1afb22506dc66dec9845
junyeong <wn8624@naver.com>
2025-12-24 17:53:21 +0900

    add 1224

---
+++ java-algorithm-interview/CH12_GRAPH/P38_조합.java
@@ -0,0 +1,24 @@
+class Solution {
+
+  public void dfs(List<List<Integer>> results, LinkedList<Integer> elements, int n, int start,
+      int k) {
+    if (k == 0) {
+      results.add(elements.stream().collect(Collectors.toList()));
+      return;
+    }
+
+    for (int i = start; i <= n; i++) {
+      elements.add(i);
+      dfs(results, elements, n, i + 1, k - 1);
+      elements.removeLast();
+    }
+  }
+
+  public List<List<Integer>> combine(int n, int k) {
+    List<List<Integer>> results = new ArrayList<>();
+    dfs(results, new LinkedList<>(), n, 1, k);
+
+    return results;
+
+  }
+}
